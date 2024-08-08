ac063eb696d913d48edf90f02f6a18121b60d9ff
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-08-08 10:47:01 +0300

    0. We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- create table users
+-- id email name
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255),
+);
