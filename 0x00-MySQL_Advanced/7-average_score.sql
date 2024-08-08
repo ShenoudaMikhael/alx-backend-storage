ba77cffc18e1ece0c2f5a4675fd94bbce9fc5caf
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-08-09 00:59:00 +0300

    7. Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,7 +1,7 @@
 -- a SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average 
 -- score for a student. Note: An average score can be a decimal
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(user_id)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
    	UPDATE users
 	SET
